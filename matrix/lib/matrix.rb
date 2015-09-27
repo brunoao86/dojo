2c68f606964c39ccb1b65b40a83bd4d952bf55e5
Bruno Oliveira <brunoao86@gmail.com>
2015-09-26 21:55:25 -0300

    adicionando classe Matrix e app/main. TODO: spec/matrix_spec.rb

---
+++ matrix/lib/matrix.rb
@@ -0,0 +1,37 @@
+class Matrix
+
+	attr_accessor :matrix, :max
+
+	def initialize(matrix)
+		@matrix = matrix
+	end
+
+	def product_line index
+		@matrix[index].inject(:*)
+	end
+
+	def product_column index
+		(0...@matrix.length).inject(1) { |prod, j| prod *= @matrix[index][j] }
+	end
+
+	def max
+		return @max unless @max.nil?
+
+		lines = []
+		columns = []
+		diagonal1 = 1
+		diagonal2 = 1
+		k = @matrix.length-1
+
+		(0...@matrix.length).each do |index|
+			lines << product_line(index)
+			columns << product_column(index)
+			diagonal1 *= @matrix[index][index]
+			diagonal2 *= @matrix[index][k]
+			k -= 1
+		end
+
+		@max = ([lines, columns, diagonal1, diagonal2]).flatten.max
+	end
+
+end
